7a21a0cde12e95c6c2441df43e9fcc7c7ae174e2
smupyknight <smupyknight@outlook.com>
2025-07-19 11:46:23 +0200

    Add package.json

--- api/[...path].ts
+++ api/[...path].ts
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
         host: undefined,
       },
       body:
-        req.method !== 'GET' && req.method !== 'HEAD' && rawBody
-          ? rawBody.toString('utf-8')
+        req.method !== 'GET' && req.method !== 'HEAD'
+          ? rawBody
           : undefined,
     });
   
