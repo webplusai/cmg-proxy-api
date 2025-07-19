fcdd5ef160781cd6f8ea006072a88f54cd018ceb
smupyknight <smupyknight@outlook.com>
2025-07-19 10:02:15 +0200

    Add proxy function

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    console.log('req.path:', JSON.stringify(req));
+    console.log('req.path:', req.pathname);
     const path = req.query.path.join('/');
     const url = `http://185.92.220.208:5000/${path}`;
   
