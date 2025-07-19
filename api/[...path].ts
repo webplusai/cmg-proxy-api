feb23b879ed5c16346aea409d8112d3ddb267414
smupyknight <smupyknight@outlook.com>
2025-07-19 09:52:30 +0200

    Add proxy function

---
+++ api/[...path].ts
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+    const path = req.query.path.join('/');
+    const url = `http://185.92.220.208:5000/${path}`;
+  
+    const response = await fetch(url, {
+      method: req.method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...req.headers,
+        host: undefined,
+      },
+      body:
+        req.method !== 'GET' && req.method !== 'HEAD'
+          ? JSON.stringify(req.body)
+          : undefined,
+    });
+  
+    const data = await response.text();
+    res.status(response.status).send(data);
+}
